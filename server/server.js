a8e687f74227159a56bf76a51c57b7ed9cde428f
Alejandro <alejandro.guevara.esc@gmail.com>
2017-11-18 22:39:43 -0600

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -45,9 +45,26 @@ app.get('/todos/:id', (req, res) => {
     if (!todo) {
       res.status(404).send();
     }
-    res.send({todo});
+    res.status(200).send({todo});
   }).catch((e) => {
     res.status(404).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+    }
+    res.status(200).send(todo);
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
